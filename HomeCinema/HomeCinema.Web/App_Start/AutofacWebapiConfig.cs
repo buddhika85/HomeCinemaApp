cd3e94b193a2ffab63d102f1faef35a8b9cf376b
buddhika85 <buddhika2006system@gmail.com>
2017-06-08 15:42:37 +0530

    Added web project

---
+++ HomeCinema/HomeCinema.Web/App_Start/AutofacWebapiConfig.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using HomeCinema.Data.Configurations;
+using HomeCinema.Data.Infrastructure;
+using HomeCinema.Data.Repositories;
+using HomeCinema.Services;
+using HomeCinema.Services.Abstract;
+using System.Data.Entity;
+using System.Reflection;
+using System.Web.Http;
+
+namespace HomeCinema.Web.App_Start
+{
+    public class AutofacWebapiConfig
+    {
+
+        public static IContainer Container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Intialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // EF Home cinema context
+            builder.RegisterType<HomeCinemaContext>().As<DbContext>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterGeneric(typeof(EntityBaseRepository<>)).As(typeof(IEntityBaseRepository<>)).InstancePerRequest();
+
+            // Services 
+            builder.RegisterType<EncryptionService>().As<IEncryptionService>().InstancePerRequest();
+            builder.RegisterType<MembershipService>().As<IMembershipService>().InstancePerRequest();
+            Container = builder.Build();
+
+            return Container;
+        }
+
+        private static void Intialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
---
+++ HomeCinema/HomeCinema.Web/App_Start/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace HomeCinema.Web.App_Start
+{
+    public class Bootstrapper
+    {
+        public static void Run()
+        {
+            // configure autofac
+            AutofacWebapiConfig.Initialize(GlobalConfiguration.Configuration);
+
+            // configure automapper
+            //AutoMapperConfiguration.Configure();
+        }
+    }
+}
